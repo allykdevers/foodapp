9cc6814127e57a33a6b0d5a77c4d0bb3a3757697
allysonkdevers <allydevers@gmail.com>
2017-06-28 13:16:42 -0400

    final commit-fixed brackets

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ $(document).ready(function () {
 
    function showResults (data) {
         var items= data.recipes;
-        for(var i = 0; i < items.length; i++) {
- $('.results').append('<div class="food"><img class="pic"src="'+ items[i].image_url+'"><a href="'+items[i].source_url+'">'+items[i].title+'</a></div></div>')
+        for(var i = 0; i < items.length; i++){
+            $('.recipe').append('<div class="food"><img class="pic"src="'
+             + items[i].image_url+'"><a href="'
+            +items[i].source_url+'">'
+            +items[i].title
+            +'</a></div>')
         }
    }
-  
+   
 function getRecipes(searchTerm){
         var params = {
             q:searchTerm,
-            
-        },
+
+            },
         url='http://localhost:3000/food';
 	$.getJSON(url,params,function (data) {
         console.log (data);
 		showResults(data);
 	});
-
-    }
+}
 $('#search-form').submit(function (event){
-    alert('submitted');
-    $('.results').html('');
+    $('.recipes').html('');
     var searchTerm = $('#search-text').val();
-    alert(searchTerm);
     getRecipes(searchTerm);
-    
     event.preventDefault();
 });
-});
+})
